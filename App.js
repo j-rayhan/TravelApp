bdcc2a84c267ddd99c90b73b1d1731a474f3452c
johir-rayhan <johir.rayhan01@gmail.com>
2021-06-03 20:35:58 +0600

    Project setup

---
+++ App.js
@@ -0,0 +1,117 @@
+import React from "react";
+import {
+    Image,
+    TouchableOpacity
+} from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
+
+// screens
+import { Onboarding, DestinationDetail } from "./src/screens";
+// extra screens
+import Tabs from "./src/navigation/tabs";
+
+import { icons, COLORS, SIZES } from './src/constants';
+import { ROUTES } from "./src/navigation/Routes";
+
+const theme = {
+    ...DefaultTheme,
+    colors: {
+        ...DefaultTheme.colors,
+        border: "transparent",
+    },
+};
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <NavigationContainer theme={theme}>
+            <Stack.Navigator
+                initialRouteName={ROUTES.ONBOARDING_SCREEN}
+            >
+                {/* Screens */}
+                <Stack.Screen
+                    name={ROUTES.ONBOARDING_SCREEN}
+                    component={Onboarding}
+                    options={{
+                        title: null,
+                        headerStyle: {
+                            backgroundColor: COLORS.white
+                        },
+                        headerLeft: null,
+                        headerRight: () => (
+                            <TouchableOpacity
+                                style={{ marginRight: SIZES.padding }}
+                                onPress={() => console.log("Pressed")}
+                            >
+                                <Image
+                                    source={icons.barMenu}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                    }}
+                />
+
+                <Stack.Screen
+                    name={ROUTES.DESTINATION_SCREEN}
+                    component={DestinationDetail}
+                    options={{ headerShown: false }}
+                />
+
+                {/* Tabs */}
+                < Stack.Screen
+                    name={ROUTES.HOME_SCREEN}
+                    component={Tabs}
+                    options={{
+                        title: null,
+                        headerStyle: {
+                            backgroundColor: COLORS.white
+                        },
+                        headerLeft: ({ onPress }) => (
+                            <TouchableOpacity
+                                style={{ marginLeft: SIZES.padding }}
+                                onPress={onPress}
+                            >
+                                <Image
+                                    source={icons.back}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                        headerRight: () => (
+                            <TouchableOpacity
+                                style={{ marginRight: SIZES.padding }}
+                                onPress={() => console.log("Menu")}
+                            >
+                                <Image
+                                    source={icons.menu}
+                                    resizeMode="contain"
+                                    style={{
+                                        width: 25,
+                                        height: 25,
+                                    }}
+                                />
+                            </TouchableOpacity>
+                        ),
+                    }}
+                />
+
+
+            </Stack.Navigator>
+        </NavigationContainer >
+    );
+};
+
+export default () => {
+    return <App />;
+};
---
+++ src/screens/Onboarding.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Button, Text, View} from 'react-native';
+import { ROUTES } from '../navigation/Routes';
+
+export default ({navigation}) => {
+  return (
+    // eslint-disable-next-line react-native/no-inline-styles
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Welcome to Onboarding Screen!</Text>
+      <Button onPress={() => navigation.navigate(ROUTES.HOME_SCREEN)} title="Go to home" />
+    </View>
+  );
+};
